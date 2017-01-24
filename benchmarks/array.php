f9eebe5258644dd989951de3ffcd53207c658e5d
irpsv <i.rpsv@live.com>
2017-01-24 22:29:11 +0500

    remove benchmark framework & clear files

--- benchmarks/array.php
+++ benchmarks/array.php
@@ -1,10 +1,21 @@
 <?php
 
+/*
+Результаты теста:
+
+Count: 1048576
+Running tests 10 times.
+Testing 3/3 : ArrayyedArray
+
+Test               Time   Time (%)   Memory   Memory (%)
+SplFixedArray    493 ms                 0 B
+Array            653 ms       32 %      0 B
+JArray          3484 ms      607 %      0 B
+
+ */
+
 // composer vendor autoload
 include __DIR__ .'/../../../autoload.php';
-
-// benckmark
-include __DIR__ .'/../vendor/autoload.php';
 
 use jugger\ds\Ds;
 use Lavoiesl\PhpBenchmark\Benchmark;
